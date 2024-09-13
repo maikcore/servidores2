6a26ec1dee6cbf50db7b62f6dfe9c909a02630a6
Miguel E. López-Muñoz <99059316+maikcore@users.noreply.github.com>
2024-09-12 20:24:12 -0700

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SERVIDORES 2 ISI UNISON
         </p>
         <a
           className="App-link"
